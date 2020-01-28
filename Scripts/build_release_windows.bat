fdf37a1b90a4ce991bea02eb3f85b21811897efa
Dmitry Khrykin <khrykin@me.com>
2020-01-28 18:55:25 +0300

    Remove unnecesarry JUCE folders

--- Scripts/build_release_windows.bat
+++ Scripts/build_release_windows.bat
@@ -1,6 +1,6 @@
 @echo OFF 
 devenv "%~dp0..\Builds\VisualStudio2019\BlackFace.sln" /build "Release|x64"
-devenv "%~dp0..\Builds\VisualStudio2019\BlackFace.sln" /build "Release (x86)|Win32"
+devenv "%~dp0..\Builds\VisualStudio2019\BlackFace.sln" /build "Release|Win32"
 
 iscc "%~dp0..\Releases\Windows\BlackFace-Win32.iss"
 iscc "%~dp0..\Releases\Windows\BlackFace-x64.iss"
--- Scripts/release_latest.sh
+++ Scripts/release_latest.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-Scripts/release.sh "$(Scripts/repo_name.sh)" "$(git describe)" -- "./Releases/MacOS/build/BlackFace.pkg"
+Scripts/release.sh "$(Scripts/repo_name.sh)" \
+"$(git describe)" -- \
+ "./Releases/MacOS/build/BlackFace.pkg" \
+ "./Releases/Windows/Output/BlackFace-windows-x64.exe" \
+ "./Releases/Windows/Output/BlackFace-windows-x86.exe"
