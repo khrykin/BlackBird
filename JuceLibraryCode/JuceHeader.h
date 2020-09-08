f2a512fe92e3d10023712ce3938f118105b9e6a8
Dmitry Khrykin <khrykin@me.com>
2020-09-08 11:17:04 +0300

    v0.1.0 Release

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BlackFace";
     const char* const  companyName    = "Dmitry Khrykin";
-    const char* const  versionString  = "0.0.1";
-    const int          versionNumber  = 0x1;
+    const char* const  versionString  = "0.1.0";
+    const int          versionNumber  = 0x100;
 }
 #endif
